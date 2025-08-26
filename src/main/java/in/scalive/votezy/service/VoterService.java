3d12ed2b174b9aeab084f940467f2f818b49fe11
ayushsahu007 <ayushsahu007ay@gmail.com>
2025-08-26 13:22:11 +0530

    add Voter Service Logic

---
+++ src/main/java/in/scalive/votezy/service/VoterService.java
@@ -0,0 +1,69 @@
+package in.scalive.votezy.service;
+
+import in.scalive.votezy.entity.Candidate;
+import in.scalive.votezy.entity.Vote;
+import in.scalive.votezy.entity.Voter;
+import in.scalive.votezy.exception.DuplicateResourceException;
+import in.scalive.votezy.exception.ResourceNotFoundException;
+import in.scalive.votezy.repository.CandidateRepository;
+import in.scalive.votezy.repository.VoterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VoterService {
+    private VoterRepository voterRepository;
+    private CandidateRepository candidateRepository;
+
+    public VoterService(VoterRepository voterRepository, CandidateRepository candidateRepository) {
+        this.voterRepository = voterRepository;
+        this.candidateRepository = candidateRepository;
+    }
+
+   public Voter registerVoter(Voter voter){
+       if (voterRepository.existsByEmail(voter.getEmail())){
+           throw new DuplicateResourceException("Voter With email id:"+voter.getEmail()+" already exists");
+       }
+       return voterRepository.save(voter);
+    }
+
+    public List<Voter>getAllVoters(){
+        return voterRepository.findAll();
+    }
+
+    public Voter getVoterById(Long id){
+        Voter voter = voterRepository.findById(id).orElse(null);
+        if (voter == null){
+            throw new ResourceNotFoundException("Voter with id"+voter.getId()+" not found");
+        }
+        return voter;
+    }
+    public Voter updateVoter(Long id,Voter updatedVoter){
+        Voter voter = voterRepository.findById(id).orElse(null);
+        if (voter==null){
+            throw new ResourceNotFoundException("Voter with id"+id+" not found");
+        }
+        voter.setName(updatedVoter.getName());
+        voter.setEmail(updatedVoter.getEmail());
+        return voterRepository.save(voter);
+    }
+
+    @Transactional
+    public void deleteVoter(Long id){
+        Voter voter = voterRepository.findById(id).orElse(null);
+        if (voter == null){
+            throw new ResourceNotFoundException("Cannot delete voter with id :"+id+"Id not exists");
+        }
+        Vote vote = voter.getVote();
+        if (vote!= null){
+            Candidate candidate = vote.getCandidate();
+            candidate.setVoteCount(candidate.getVoteCount()-1);
+            candidateRepository.save(candidate);
+        }
+        voterRepository.delete(voter);
+    }
+}
